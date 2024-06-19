be971eab496587aaee6905310558f66247df369b
bhavnabarnwal <123322330+bhavnabarnwal@users.noreply.github.com>
2024-06-19 11:47:17 +0530

    Update script.js

--- project-5 weather app/script.js
+++ project-5 weather app/script.js
@@ -12,7 +12,7 @@ const weather_body = document.querySelector('.weather-body');
 
 
 async function checkWeather(city){
-    const api_key = "13d39784ff8e03e0f459d7ea28af7877";
+    const api_key = "Weather_API_Key";
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}`;
 
     const weather_data = await fetch(`${url}`).then(response => response.json());
